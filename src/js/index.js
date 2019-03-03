adf17d3efb70efb8b98735035127070f3bd919eb
aso777 <safonand@gmail.com>
2019-03-03 11:15:07 -0800

    Added state to the application.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,38 @@
 import Search from './models/Search';
+
+/**
+ * Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+    //Get query from a view
+    const query = 'pizza';
+
+    if (query) {
+        // New search object
+        state.search = new Search(query);
+
+        // Prepare UI
+
+        // Search for recipes
+        await state.search.getRecipes();
+
+        //Render the results
+        console.log(state.search.results);
+    }
+
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
 const search = new Search('pizza');
 search.getRecipes();
 console.log(search);
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -7,6 +7,5 @@ export default class Search {
         const key = '771cca0a9df01bf1a3441d8bf539a8b8';
         const response = await fetch(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
         this.results = await response.json();
-        console.log(this.results);
     }
 }
