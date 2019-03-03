60262caba2c4bbfaf7d853a4251bdcb0658a3491
aso777 <safonand@gmail.com>
2019-03-03 11:02:53 -0800

    Implemented search async function.

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,4 @@
-import string from './models/Search';
-console.log(string);
-
-import {
-    add,
-    multiply,
-    ID
-} from './views/searchView';
-console.log(`using imported function! ${add(ID, 2)} and ${multiply(ID, 3)}`);
-
-
-//API key: 771cca0a9df01bf1a3441d8bf539a8b8
-
-/*
-https://www.food2fork.com/api/search
-All recipe requests should be made to the recipe details API URL.
-
-https://www.food2fork.com/api/get 
-*/
-const key = '771cca0a9df01bf1a3441d8bf539a8b8'
-
-async function getRecipes(q) {
-    const response = await fetch(`https://www.food2fork.com/api/search?key=${key}&q=${q}`)
-    const jsonRes = await response.json();
-    console.log(JSON.stringify(jsonRes));
-}
-getRecipes("Chicken");
+import Search from './models/Search';
+const search = new Search('pizza');
+search.getRecipes();
+console.log(search);
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,12 @@
-export default 'I am an exported string';
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getRecipes() {
+        const key = '771cca0a9df01bf1a3441d8bf539a8b8';
+        const response = await fetch(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
+        this.results = await response.json();
+        console.log(this.results);
+    }
+}
